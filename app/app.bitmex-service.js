fc3e34381b9727ba17dbcf3845f72ce35f6ec3aa
Fullservice Fullstacked <honey96dev@gmail.com>
2019-06-04 17:41:31 +0300

    Update app.bitmex-service.js

--- app/app.bitmex-service.js
+++ app/app.bitmex-service.js
@@ -1065,8 +1065,10 @@ service.calculateHiddenOrders2 = (timestamp) => {
         }
 
         let requestData = JSON.parse(body);
-        timestamp = new Date(requestData.timestamp).toISOString();
+        timestamp = new Date(new Date(requestData.timestamp).setSeconds(0, 0));
+        timestamp = timestamp.toISOString();
         console.warn('hidden', timestamp);
+        return;
 
         let sql = sprintf("SELECT '%s' `timestamp`, H.symbol, H.side, SUM(H.size) `size`, SUM(H.price) `price`, H.tickDirection, H.trdMatchID, SUM(H.grossValue) `grossValue`, SUM(H.homeNotional) `homeNotional`, SUM(H.foreignNotional) `foreignNotional` FROM `hidden_orders` H WHERE `timestamp` LIKE '%s%s' GROUP BY `side`;", timestamp, timestamp.substr(0, 16), '%');
         console.log(sql);
